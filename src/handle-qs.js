a23f33a1533adba9e653cc548e0d2dd19af6c7bf
Forbes Lindesay <forbes@lindesay.co.uk>
2016-10-23 13:39:03 +0100

    Add missing file

---
+++ src/handle-qs.js
@@ -0,0 +1,36 @@
+import {parse, stringify} from 'qs';
+
+export default function handleQs(url, query) {
+  url = url.split('?');
+  const start = url[0];
+  let qs = (url[1] || '').split('#')[0];
+  const end = url[1] && url[1].split('#').length > 1 ? '#' + url[1].split('#')[1] : '';
+
+  const baseQs = parse(qs);
+  for (const i in query) {
+    baseQs[i] = query[i];
+  }
+  qs = stringify(baseQs);
+  if (qs !== '') {
+    qs = '?' + qs;
+  }
+  return start + qs + end;
+}
+export function removeFields(url, fields) {
+  url = url.split('?');
+  const start = url[0];
+  let qs = (url[1] || '').split('#')[0];
+  const end = url[1] && url[1].split('#').length > 1 ? '#' + url[1].split('#')[1] : '';
+
+  const baseQs = parse(qs);
+  fields.forEach(key => {
+    if (key in baseQs) {
+      delete baseQs[key];
+    }
+  });
+  qs = stringify(baseQs);
+  if (qs !== '') {
+    qs = '?' + qs;
+  }
+  return start + qs + end;
+}
